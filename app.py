ad3f56a5ad5ddb0ddc4d3adf8a54a234c04d568d
is13zg <is13mozg@gmail.com>
2025-08-11 16:51:57 +0300

    add calc

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from  fastapi.responses import HTMLResponse
 import uvicorn
+
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return {"message": "Добро пожаловать в моё приложение FastAPI!"}
+    with open("index.html", "r", encoding="utf-8") as f:
+        data = f.read()
+        return data
 
+@app.post("/calculate")
+async def calc(num1: int, num2: int):
+    return {"num1": num1, "num2": num2, "result":num1 + num2}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", reload=True)
