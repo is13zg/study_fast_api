c543b6f6f7054d5872feed75a85f5d4cc48a9502
is13zg <is13mozg@gmail.com>
2025-08-13 17:55:33 +0300

    work with jwt token

--- login.py
+++ login.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter, Response, Cookie, Request
 from models import UserLogin, MessageResponse
 from hashlib import sha256
 from uuid import uuid4
+import jwt
 
+secret = "my_secret_string"
 login_router = APIRouter()
 
 users_db = [UserLogin(username="Vasya", password=sha256("paasword1".encode()).hexdigest()),
@@ -14,16 +16,34 @@ users_db = [UserLogin(username="Vasya", password=sha256("paasword1".encode()).he
 sessions: dict = {}  # это можно хранить в кэше, например в Redis
 
 
+
 @login_router.post("/login", tags=["login", ])
 async def login(user_login: UserLogin, response: Response) -> MessageResponse:
     for user in users_db:
         if user.username == user_login.username:
             if user.password == sha256(user_login.password.encode()).hexdigest():
-                session_token = uuid4()
-                response.set_cookie(key="session_token", value=session_token, httponly=True)
-                sessions[str(session_token)] = user
+                user_id = {"user_id": str(uuid4())}
+                session_token = jwt.encode(user_id, secret, algorithm="HS256")
+                response.set_cookie(key="session_token", value=session_token, max_age=10, httponly=True)
+                sessions[user_id["user_id"]] = user
                 return {"message": f"Login sucses."}
     return {"message": f"User {user_login.username} not registred."}
+
+
+@login_router.get("/profile", tags=["login", ])
+async def user_info(request: Request):
+    session_token = request.cookies.get("session_token")
+    print(request.cookies)
+
+    try:
+        decoded = jwt.decode(session_token, secret, algorithms=["HS256"])
+        user = sessions.get(decoded["user_id"])
+        if user:
+            return user
+    except jwt.InvalidSignatureError as e:
+        return {"message": f"{e}"}
+    except Exception:
+        print("no cookie")
 
 
 @login_router.get("/check_user", tags=["login", ])
