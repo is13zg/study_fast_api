6edd5c7f7a3695208264214f5b6b91030467a858
13 <is-13mozg@inbox.ru>
2025-08-28 11:14:44 +0300

    add task on

--- 5.1.2/main.py
+++ 5.1.2/main.py
@@ -1,21 +1,22 @@
 from fastapi import FastAPI, Depends
-from pydantic import BaseModel
 from db.database import get_db_connection
 import asyncpg
 import uvicorn
+from models import Todo
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
 
-@app.post("/items")
-async def create_item(item: Item, db: asyncpg.Connection = Depends(get_db_connection)):
-    await db.execute('''
-        INSERT INTO items(name) VALUES($1)
-    ''', item.name)
-    return {"message": "Item added successfully!"}
+@app.post("/add_task")
+async def create_item(item: Todo, db: asyncpg.Connection = Depends(get_db_connection)):
+    new_task = await db.fetchrow('''
+        INSERT INTO tasks (title, description, completed)
+        VALUES ($1, $2, $3)
+        RETURNING *;
+    ''', item.title, item.description, item.completed)
+
+    return dict(new_task)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload= True)
+    uvicorn.run("main:app", reload=True)
